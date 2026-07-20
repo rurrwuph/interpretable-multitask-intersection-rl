4d5ed6028966058e52ad10fecaf321799989d92a
rurrwuph <sukuna1205@gmail.com>
2026-07-20 11:24:19 +0600

    Setup RLController for ego vehicle and purge test specs

--- Network_scenario.py
+++ Network_scenario.py
@@ -42,23 +42,4 @@ vehicles.add(
 
 
 
-# initial_test_veh_type_0 = 'veh_0'
-# initial_test_veh_type_1 = 'veh_1'
-# initial_test_veh_type_2 = 'veh_2'
-# initial_test_veh_type_3 = 'veh_3'
-# initial_test_veh_type_4 = 'veh_4'
-# initial_test_veh_type_5 = 'veh_5'
-# initial_test_veh_type_6 = 'veh_6'
-# initial_test_veh_type_7 = 'veh_7'
-# initial_test_veh_type_8 = 'veh_8'
-# initial_test_veh_type_9 = 'veh_9'
-# initial_test_veh_type_10 = 'veh_10'
-# initial_test_veh_type_11 = 'veh_11'
-# initial_test_veh_type_12 = 'veh_12'
-# initial_test_veh_type_13 = 'veh_13'
-# initial_test_veh_type_14 = 'veh_14'
-# initial_test_veh_type_15 = 'veh_15'
-# initial_test_veh_type_16 = 'veh_16'
-# initial_test_veh_type_17 = 'veh_17'
-# initial_test_veh_type_18 = 'veh_18'
-# initial_test_veh_type_19 = 'veh_19'
+# ego vehicle rlcontroller configured
